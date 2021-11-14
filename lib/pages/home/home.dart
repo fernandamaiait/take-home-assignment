cede3633a4a714b39cc3d925c65c9c643be7f8e8
Fernanda <fernanda.maia.it@gmail.com>
2021-11-14 20:48:12 -0300

    Implementing home components

--- lib/pages/home/home.dart
+++ lib/pages/home/home.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:take_home_assignment/design_system/theme_colors.dart';
 import 'package:take_home_assignment/design_system/components/navbar.dart';
+import 'package:take_home_assignment/design_system/typography.dart';
+import 'package:take_home_assignment/design_system/utils.dart';
 import 'package:take_home_assignment/pages/home/components/simulation_card.dart';
 
 class Home extends StatefulWidget {
@@ -14,13 +16,42 @@ class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: ThemeColors.neutralGrey,
+      backgroundColor: ThemeColors.blueGray10,
       body: Column(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
         children: [
-          Navbar('Origin'),
-          SimulationCard(),
-          Container(),
+          Navbar(),
+          SizedBox(height: isLargeScreen(context) ? 48 : 32),
+          Column(
+            children: [
+              HomeTitle(),
+              SizedBox(height: 24),
+              SimulationCard(),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class HomeTitle extends StatelessWidget {
+  const HomeTitle({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Text.rich(
+      TextSpan(
+        text: 'Let\'s plan your ',
+        style: ThemeTypography.subtitle(
+          context,
+          color: ThemeColors.brandColorPrimary,
+        ),
+        children: <TextSpan>[
+          TextSpan(
+              text: 'saving goal.',
+              style: TextStyle(
+                fontWeight: FontWeight.w600,
+              )),
         ],
       ),
     );
