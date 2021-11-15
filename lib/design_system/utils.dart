a7c64097a8ff6dc32460bce65a975da5596317e1
Fernanda <fernanda.maia.it@gmail.com>
2021-11-14 22:15:19 -0300

    Setting the media query parameter

--- lib/design_system/utils.dart
+++ lib/design_system/utils.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
 
 bool isLargeScreen(BuildContext context) {
-  return MediaQuery.of(context).size.width > 560;
+  return MediaQuery.of(context).size.width > 480;
 }
